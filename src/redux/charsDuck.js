ee93bb10f1f760f00e03362a3c102de6b4037bdd
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-01-04 15:36:02 -0500

    more chars actions

--- src/redux/charsDuck.js
+++ src/redux/charsDuck.js
@@ -12,8 +12,12 @@ let GET_CHARACTERS = "GET_CHARACTERS";
 let GET_CHARACTERS_SUCCESS = "GET_CHARACTERS_SUCCESS";
 let GET_CHARACTERS_ERROR = "GET_CHARACTERS_ERROR";
 
+let REMOVE_CHARACTER = "REMOVE_CHARACTER";
+
 export default function reducer(state = initialData, action) {
   switch (action.type) {
+    case REMOVE_CHARACTER:
+      return { ...state, array: action.payload };
     case GET_CHARACTERS:
       return { ...state, fetching: true };
     case GET_CHARACTERS_ERROR:
@@ -24,6 +28,16 @@ export default function reducer(state = initialData, action) {
       return state;
   }
 }
+
+export let removeCharacterAction = () => (dispatch, getState) => {
+  let { array } = getState().characters;
+  array.shift();
+
+  dispatch({
+    type: REMOVE_CHARACTER,
+    payload: [...array]
+  });
+};
 
 export let getCharactersAction = () => (dispatch, getState) => {
   dispatch({ type: GET_CHARACTERS });
