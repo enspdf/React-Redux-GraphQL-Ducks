5315545c026679e2580bc7b59538e2f23bcede6f
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-01-04 00:05:33 -0500

    Characters reducer | actions

---
+++ README.md
@@ -0,0 +1,3 @@
+
+### Udemy Aprende Redux con React hooks y GraphQL fácilmente
+[Link](https://www.udemy.com/course/aprende-redux-con-react-hooks-y-graphql-facilmente)
---
+++ src/redux/userDuck.js
@@ -0,0 +1,13 @@
+let initialData = {
+    loggedIn: false
+};
+let LOGIN = "LOGIN";
+
+export default function reducer(state = initialData, action) {
+  switch (action.type) {
+      case LOGIN:
+          return 
+      default:
+          return state;
+  }
+}
